a18e85cb310f5cc11ae270a4c7e3c988217f8409
12kia29 <kiainwar@gmail.com>
2026-09-24 04:26:00 +0330

    Exercise 1 (js)1

---
+++ js/main.js
@@ -0,0 +1,66 @@
+//test-alert
+//alert("hello")
+ 
+// test-console
+console.log("hello")
+//test-console-warning
+console.warn('22warning')
+//test-console-error
+console.error('34error')
+//test-dom--change inside by attribute id
+document.getElementById("content").innerHTML = "message"
+//test-dom--write to text by dom
+document.write("counter subject")
+//variable test
+//test-var
+var spider="spider man"
+console.log(spider)
+//test-let
+let lion="lion king"
+console.log(lion)
+//test-const--use camelcase  
+const elephantBlack="big noise"
+console.log(elephantBlack)
+//test-number,null,undefined
+let score=40
+console.log(score)
+score=50
+console.log(score)
+const name=null
+console.log(name)
+console.log(typeof name)//back to object(nice bug🤣1995)
+const age=undefined 
+console.log(age)
+console.log(typeof age)
+
+// + - * / %
+const sigma=10+score
+console.log(sigma)
+const minus=score-5
+console.log(minus)
+const point=score*5
+console.log(point)
+const midScore=score/2
+console.log(midScore)
+const remain=score%3
+console.log(remain)
+//++ --
+score--
+console.log(score)
+score++
+console.log(score)
+let newScore=score++
+console.log(newScore)
+let newScore2=++score
+console.log(newScore2)
+//let newScore3=+++score
+//console.log(newScore3) //test+++
+console.log(score)
+let newScore4=score--
+console.log(newScore4)
+console.log(score)
+let newScore5=--score
+console.log(newScore5)
+
+
+
